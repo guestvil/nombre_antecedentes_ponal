bf14bec42845b056cd35e6a962bd6959ebb9eeca
Guillermo Estupinan <estupinan.services@outlook.com>
2025-02-27 19:57:14 -0500

    odofied the code to remove accents from the names. Also, implemented a more complete data structure for storing each user information

--- ponal_cedulas.py
+++ ponal_cedulas.py
@@ -1,22 +1,29 @@
+import pandas as pd
 import pymupdf
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+import unicodedata
 
-def get_cedulas(pdf_test: str):
-    '''Retuns a list of list containing: [cedula, name]'''
-    table_elements = []
-    with pymupdf.open(pdf_test) as file:
-        for page in file:
-            tab = page.find_tables()
-            tab_text = tab[0].extract()
-            for a_list in tab_text[1:]:
-                table_elements.append(a_list[:2])
-    # for elemento in table_elements:
-      #  print(elemento)
-    return table_elements
+def remove_accents(text):
+    return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
 
-def main():
-    cedulas_list = get_cedulas(pdf_test='Jurados de votación.pdf')
+
+
+def get_documents(excel_path: str):
+    total_data = []
+    old_excel_file = pd.read_excel(excel_path, header=None, engine='openpyxl')
+    old_excel_file = old_excel_file.applymap(lambda x: remove_accents(str(x)) if isinstance(x, str) else x)
+    old_excel_file.to_excel('cleaned_file.xlsx', index=False)
+    excel_file = pd.read_excel('cleaned_file.xlsx', header=None, engine='openpyxl')
+    for row in excel_file.itertuples(index=True):
+        user_data = {'id': row[1],
+                     'id_type': row[2],
+                     'name_list': str(row[3]).upper().split() + str(row[4]).upper().split()}
+        total_data.append(user_data)
+    return total_data
+
+
+def get_name_ponal(user_info: dict):
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch_persistent_context(user_data_dir='', headless=False, ignore_default_args=['--enable-automation'], args=[
         f"--disable-extensions-except=./chromium_automation",
@@ -37,6 +44,12 @@ def main():
                 name = list_of_text[index + 1]
         print(name)
     return None
+
+def main():
+    users_data = get_documents(excel_path='usuarios_wplay.xlsx')
+    print('The data list has ', len(users_data), ' elements')
+    for user_infor in users_data[1:40]:
+        print(user_infor)
 
 
 if __name__ == '__main__':
