1bf58be31a508ccb214ec3fe12429c0084db184f
Guillermo Estupinan <estupinan.services@outlook.com>
2025-02-28 19:45:49 -0500

    Deleted the option to include other types of documents, but google blocked me as it detected the automation. Best option will be to use a rotating IP service

--- ponal_cedulas.py
+++ ponal_cedulas.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pymupdf
 from playwright.sync_api import sync_playwright
+from playwright._impl._errors import TimeoutError
 from bs4 import BeautifulSoup
 import unicodedata
 
@@ -16,40 +17,72 @@ def get_documents(excel_path: str):
     old_excel_file.to_excel('cleaned_file.xlsx', index=False)
     excel_file = pd.read_excel('cleaned_file.xlsx', header=None, engine='openpyxl')
     for row in excel_file.itertuples(index=True):
-        user_data = {'id': row[1],
+        user_data = {'id': str(row[1]),
                      'id_type': row[2],
                      'name_list': str(row[3]).upper().split() + str(row[4]).upper().split()}
         total_data.append(user_data)
     return total_data
 
 
-def get_name_ponal(user_info: dict):
+def get_name_ponal(users_info: dict):
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch_persistent_context(user_data_dir='', headless=False, ignore_default_args=['--enable-automation'], args=[
         f"--disable-extensions-except=./chromium_automation",
         f"--load-extension=./chromium_automation",])
         ponal_page = browser.new_page()
+        real_names = []
         ponal_page.goto('https://antecedentes.policia.gov.co:7005/WebJudicial/')
-        ponal_page.locator("#aceptaOption\\:0").click()
-        ponal_page.get_by_role('button', name='Enviar').click()
-        ponal_page.locator('div#captchaAntecedentes').click()
-        ponal_page.wait_for_timeout(5000)
-        ponal_page.get_by_role('textbox').fill('79940745')
-        ponal_page.get_by_role('button', name='Consultar').click()
-        ponal_html = ponal_page.locator('span#form\\:mensajeCiudadano').inner_html()
-        html_bs = BeautifulSoup(ponal_html, 'html.parser')
-        list_of_text = list(html_bs.stripped_strings)
-        for index, text in enumerate(list_of_text):
-            if text.strip() == 'Apellidos y Nombres:':
-                name = list_of_text[index + 1]
-        print(name)
-    return None
+        for user_info in users_info:
+            if user_info['id_type'] == 'PEP':
+                continue
+            ponal_page.locator("#aceptaOption\\:0").click()
+            ponal_page.get_by_role('button', name='Enviar').click()
+            try: 
+                # ponal_page.wait_for_timeout(10000)
+                # ponal_page.locator('div#captchaAntecedentes').click()
+                # Locate the iframe first
+                captcha_iframe = ponal_page.frame_locator('iframe[title="reCAPTCHA"]')
+                # Wait until the checkmark appears inside the iframe
+                captcha_iframe.locator('.recaptcha-checkbox-checkmark').wait_for(state='visible', timeout=40000)  # Max 35 sec
+            except TimeoutError as e:
+                print(e)
+                ponal_page.goto('https://antecedentes.policia.gov.co:7005/WebJudicial/')
+                continue
+            # ponal_page.wait_for_timeout(5000)
+            if user_info['id_type'] != 'CC' and user_info['id_type'] != 'PEP':
+                if user_info['id_type'] == 'PPT':
+                    id_type = 'pa'
+                if user_info['id_type'] == 'CE': 
+                    id_type = 'cs'
+                ponal_page.locator('select#cedulaTipo').select_option(id_type)
+            ponal_page.get_by_role('textbox').fill(user_info['id'])
+            ponal_page.get_by_role('button', name='Consultar').click()
+            ponal_html = ponal_page.locator('span#form\\:mensajeCiudadano').inner_html()
+            html_bs = BeautifulSoup(ponal_html, 'html.parser')
+            list_of_text = list(html_bs.stripped_strings)
+            for index, text in enumerate(list_of_text):
+                if text.strip() == 'Apellidos y Nombres:':
+                    name = list_of_text[index + 1]
+                    name = name.upper().split()
+                    user_real_info = {'id': user_info['id_type'], 'name': name}
+                    break
+                else: 
+                    user_info = None
+                    continue
+            if user_info != None: 
+                real_names.append(user_real_info)
+            ponal_page.get_by_role('button', name='Volver al inicio').click()
+    return real_names
+
 
 def main():
     users_data = get_documents(excel_path='usuarios_wplay.xlsx')
     print('The data list has ', len(users_data), ' elements')
-    for user_infor in users_data[1:40]:
-        print(user_infor)
+    for user_info in users_data[4:40]:
+        print(user_info)
+    real_names = get_name_ponal(users_data[4:40])
+
+    
 
 
 if __name__ == '__main__':
